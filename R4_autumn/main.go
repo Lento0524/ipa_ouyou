2a0eaab434ace1d5e2b8a2f0ac85d4d4caa6a2ee
Lento0524 <sunflower.dandelion.0524.hart@gmail.com>
2023-01-25 23:02:35 +0900

    ウの直前まで

--- R4_autumn/main.go
+++ R4_autumn/main.go
@@ -2,14 +2,16 @@ package main
 
 var goal_x, goal_y int
 var stack_top int
+var sol_num int
 
 func main() {
 	stack_top = 0
-	sol_num := 0
+	sol_num = 0
 	setMaze2()
 	showMaze()
 	start_x, start_y := 1, 1
 	goal_x, goal_y = 5, 5
+	paths = map[int]map[int]point{}
 	visit(start_x, start_y)
 
 }
--- R4_autumn/visit.go
+++ R4_autumn/visit.go
@@ -6,12 +6,51 @@ const OK flag = 1
 const NG flag = 0
 const VISITED flag = 2
 
+var stack_visit map[int]point
+var paths map[int]map[int]point
+
 func visit(x, y int) {
+	paths[sol_num] = map[int]point{}
 	maze[x][y] = VISITED
-	//stack_visit[stack_top] <- (x,y)
+	stack_visit[stack_top] = point{
+		x: x,
+		y: y,
+	}
 	if x == goal_x && y == goal_y {
 		for k := 0; k <= stack_top; k++ {
-			//paths[sol_num][k] <-stack_visit[k]
+			paths[sol_num][k] = stack_visit[k]
 		}
+		sol_num += 1
+	} else {
+		stack_top += 1
+		if maze[x][y+1] == OK {
+			visit(x, y+1)
+		}
+		if maze[x+1][y] == OK {
+			visit(x+1, y)
+		}
+		if maze[x][y-1] == OK {
+			visit(x, y-1)
+		}
+		if maze[x-1][y] == OK {
+			visit(x-1, y)
+		}
+
 	}
+
 }
+
+type point struct {
+	x int
+	y int
+}
+
+//class
+
+//public class zahyou{
+//	int x;
+//	int y;
+//
+//	p
+//
+//}
